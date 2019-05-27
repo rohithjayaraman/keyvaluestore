f714862429563102c99b4a3fc6a13119c06fb419
parselmouth <iamrj19@gmail.com>
2019-05-27 19:39:01 +0200

    Update pom.xml

--- src/test/java/ValidPathKeyValueStoreCreationTest.java
+++ src/test/java/ValidPathKeyValueStoreCreationTest.java
@@ -2,10 +2,13 @@ import kvstore.InvalidPathException;
 import kvstore.KeyValueStore;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ValidPathKeyValueStoreCreationTest {
     @Test
     public void testX() throws InvalidPathException
     {
         KeyValueStore kv = new KeyValueStore("C:/");
+        assertEquals(true,kv.apocalypseNow());
     }
 }
